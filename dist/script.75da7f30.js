4a6aea84a101cc29e1b910faa7dd42bca4375c69
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-11-13 08:37:05 +0300

    Trying to add the list item

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -30108,7 +30108,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58977" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60640" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
