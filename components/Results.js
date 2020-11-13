513361b2ba979bd69ad7be6ffdac4e3845388bee
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-11-13 09:29:43 +0300

    Change the view of the score

--- components/Results.js
+++ components/Results.js
@@ -10,7 +10,7 @@ function Results({goodGuess, setGoodGuess, setIsCorrect, fetchData}) {
                 <h2>Results</h2>
             </header>
             <p>
-                You got <span>{goodGuess}</span> correct answers
+                You got <span className="score">{goodGuess}</span> correct answers
             </p>
             <button
                 className="recommendation-btn"
