74db5aa4e6880a734aac20ff0946028dd89ac4cf
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-03-17 15:11:52 +0300

    Align the items inside of the button in the center

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -30149,7 +30149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54444" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55793" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
